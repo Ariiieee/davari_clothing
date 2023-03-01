dee89e4d687309508a2c5fcc94b771fafc6c5177
Ariiie <oluwafunmilayodada2018@gmail.com>
2023-03-01 18:33:17 +0100

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import {Routes, Route, Outlet} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.jsx";
+import Navbar from "./components/routes/navbar/Navbar.jsx";
 
 const App = () => {
-	const Navigation = () => {
-		return (
-			<div>
-				<div>
-					<h1>This navigation</h1>
-				</div>
-				<Outlet />
-			</div>
-		);
-	};
 	const Shop = () => {
 		return <div>This is a shop</div>;
 	};
 	return (
 		<Routes>
-			<Route path="/" element={<Navigation />}>
+			<Route path="/" element={<Navbar />}>
 				<Route index element={<Home />} />
 				<Route path="shop" element={<Shop />} />
 			</Route>
